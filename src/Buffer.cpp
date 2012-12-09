763cd5ef9c8bcd60d25cef97897cbd683c759546
Hrishikesh Amur <amur@gatech.edu>
2012-12-09 15:33:07 -0500

    Perform reduction on GPU

--- src/Buffer.cpp
+++ src/Buffer.cpp
@@ -32,8 +32,8 @@
 namespace gpucbt {
     class CompressTree;
 
-    const uint32_t Buffer::kMaximumElements = 10000000;
-    const uint32_t Buffer::kEmptyThreshold = 5000000;
+    const uint32_t Buffer::kMaximumElements = 40000000;
+    const uint32_t Buffer::kEmptyThreshold = 20000000;
 
     Buffer::Buffer() :
             messages_(NULL),
@@ -215,7 +215,7 @@ namespace gpucbt {
 
     bool Buffer::Aggregate(bool use_gpu) {
         bool ret;
-        if (false/*use_gpu*/) {
+        if (use_gpu) {
             ret = GPUAggregate();
         } else {
             ret = CPUAggregate();
--- src/BufferUtils.cu
+++ src/BufferUtils.cu
@@ -29,19 +29,23 @@ namespace gpucbt {
             fprintf(stderr, "Some other error: %s\n", e.what());
             exit(-1);
         }
-        // copy sorted hashes back
-        thrust::copy(d_hash.begin(), d_hash.end(), hashes_);
-
         thrust::copy(d_perm.begin(), d_perm.end(), perm_);
 
-/*
+        // copy over messages
+        thrust::device_vector<Message> d_msg(messages_, messages_ + num);
+        thrust::device_vector<Message> d_agg_msg(num);
+        // gather messages according to permutations
+        thrust::gather(d_perm.begin(), d_perm.end(), d_msg.begin(),
+                d_agg_msg.begin());
+
         typedef thrust::device_vector<uint32_t>::iterator it_h;
-        typedef thrust::device_vector<uint32_t>::iterator it_p;
-        thrust::pair<it_h, it_p> new_end;
+        typedef thrust::device_vector<Message>::iterator it_m;
+        thrust::pair<it_h, it_m> new_end;
 
         try {
             new_end = thrust::reduce_by_key(d_hash.begin(), d_hash.end(),
-                    d_msg.begin(), d_hash.begin(), d_msg.begin(),
+                    d_agg_msg.begin(), d_hash.begin(), d_agg_msg.begin(),
+                    thrust::equal_to<int>(),
                     Message::MessageMerge());
         } catch(std::bad_alloc &e) {
             fprintf(stderr, "Ran out of memory while sorting\n");
@@ -50,7 +54,10 @@ namespace gpucbt {
             fprintf(stderr, "Some other error: %s\n", e.what());
             exit(-1);
         }
-*/
+        // copy sorted hashes back
+        thrust::copy(d_hash.begin(), new_end.first, hashes_);
+        thrust::copy(d_agg_msg.begin(), new_end.second, messages_);
+        set_num_elements(new_end.first - d_hash.begin());
     }
 
     bool Buffer::GPUAggregate() {
