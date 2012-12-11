f3ca0615c865bfeb201ac0ad39214298bc3ee6c0
Hrishikesh Amur <amur@gatech.edu>
2012-12-11 18:58:39 -0500

    Remove the use of permutation pointers when aggregating on GPUs; they're useless

--- src/BufferUtils.cu
+++ src/BufferUtils.cu
@@ -15,13 +15,13 @@ namespace gpucbt {
         assert(hashes_[num - 1]);
         assert(sizeof(messages_[num - 1]));
 
+        // copy over hashes
         thrust::device_vector<uint32_t> d_hash(hashes_, hashes_ + num);
-        thrust::device_vector<uint32_t> d_perm(num);
-
-        thrust::sequence(d_perm.begin(), d_perm.begin() + num);
+        // copy over messages
+        thrust::device_vector<Message> d_msg(messages_, messages_ + num);
 
         try {
-            thrust::sort_by_key(d_hash.begin(), d_hash.end(), d_perm.begin());
+            thrust::sort_by_key(d_hash.begin(), d_hash.end(), d_msg.begin());
         } catch(std::bad_alloc &e) {
             fprintf(stderr, "Ran out of memory while sorting\n");
             exit(-1);
@@ -29,14 +29,6 @@ namespace gpucbt {
             fprintf(stderr, "Some other error: %s\n", e.what());
             exit(-1);
         }
-        thrust::copy(d_perm.begin(), d_perm.end(), perm_);
-
-        // copy over messages
-        thrust::device_vector<Message> d_msg(messages_, messages_ + num);
-        thrust::device_vector<Message> d_agg_msg(num);
-        // gather messages according to permutations
-        thrust::gather(d_perm.begin(), d_perm.end(), d_msg.begin(),
-                d_agg_msg.begin());
 
         typedef thrust::device_vector<uint32_t>::iterator it_h;
         typedef thrust::device_vector<Message>::iterator it_m;
@@ -44,7 +36,7 @@ namespace gpucbt {
 
         try {
             new_end = thrust::reduce_by_key(d_hash.begin(), d_hash.end(),
-                    d_agg_msg.begin(), d_hash.begin(), d_agg_msg.begin(),
+                    d_msg.begin(), d_hash.begin(), d_msg.begin(),
                     thrust::equal_to<int>(),
                     Message::MessageMerge());
         } catch(std::bad_alloc &e) {
@@ -56,7 +48,7 @@ namespace gpucbt {
         }
         // copy sorted hashes back
         thrust::copy(d_hash.begin(), new_end.first, hashes_);
-        thrust::copy(d_agg_msg.begin(), new_end.second, messages_);
+        thrust::copy(d_msg.begin(), new_end.second, messages_);
         set_num_elements(new_end.first - d_hash.begin());
     }
 
